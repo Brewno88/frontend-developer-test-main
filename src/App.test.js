5cc1b13d6de4227d9acd6dc152b2f3bff250c927
Vincenzo Conte <vincenzo.conte@atkinsglobal.com>
2022-04-19 17:27:37 +0100

    Add and Fix Tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,12 @@ import App from './App';
 
 test('renders app title link', () => {
   render(<App />);
-  const titleElement = screen.getByText(/Todo App/i);
+  const titleElement = screen.getByText(/To do App/i);
   expect(titleElement).toBeInTheDocument();
 });
 
 test('render Clear All button', () => {
-
-})
+  render(<App />);
+  const clearAllButton = screen.getByText(/Clear All/i);
+  expect(clearAllButton).toBeInTheDocument();
+});
